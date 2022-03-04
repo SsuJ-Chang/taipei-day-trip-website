f0868afb8efa7da72801a3b60c2e7447cdda288d
RJ Chang <loch0728@gmail.com>
2022-03-04 21:53:29 +0800

    Add images for index.html

--- public/keyword.js
+++ public/keyword.js
@@ -3,8 +3,8 @@ window.addEventListener("load", ()=>{
     let query=document.getElementById("queryBtn")
     query.addEventListener("click", ()=>{
         let queryInput=document.getElementById("queryKeyword").value;  // 接收 input
-        // let src="http://192.168.50.177:3000/api/attractions?keyword="+queryInput
-        let src="http://18.213.157.118:3000/api/attractions?keyword="+queryInput
+        let src="http://192.168.50.177:3000/api/attractions?keyword="+queryInput
+        // let src="http://18.213.157.118:3000/api/attractions?keyword="+queryInput
         fetch(src).then((response)=>{
             return response.json();  // 取得回應物件並轉為 JSON 格式
         }).then((data)=>{
