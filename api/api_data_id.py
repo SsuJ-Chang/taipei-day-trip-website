6d34f5284c2240d7aebc71ccb81d4a574451418a
RJ Chang <loch0728@gmail.com>
2022-03-14 06:05:17 +0800

    Fix wrong process when cannot fetch data

--- api/api_data_id.py
+++ api/api_data_id.py
@@ -20,15 +20,18 @@ def getData(attractionId):
     data_cursor=cnx.cursor(buffered=False, dictionary=True)
     data_cursor.execute("SELECT * FROM `tpe-attractions` WHERE id=%s", (attractionId,))
     result=data_cursor.fetchone()
-    temp_images_val=json.loads(result['images']) # 把 images 的 value 從 字串 轉回 list
-    result['images']=temp_images_val
+    print(result)
     data_cursor.close()
     try:
         if result != None:
+            temp_images_val=json.loads(result['images']) # 把 images 的 value 從 字串 轉回 list
+            result['images']=temp_images_val
             result={
                 "data":result
             }
             status=200
+
+            print(f"查到 id={result['data']['id']} 的資料", result['data']['name'])
         # elif result == None: # 景點編號不正確
         else:
             result={
@@ -37,15 +40,13 @@ def getData(attractionId):
             }
             status=400
     except 500: # 伺服器內部錯誤
-        final_result={
+        result={
 			"error":True,
 			"message":"伺服器內部錯誤"
 		}
         status=500
 
     cnx.close()
-    
-    print(f"查到 id={result['data']['id']} 的資料", result['data']['name'])
 
     # final_result=jsonify(result)
     response=make_response(result, status, {"content-type":"application/json"})
